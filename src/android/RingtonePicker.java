c7e38110d2435399354d0d26df276b99100b629e
sanalt <sanal.thomas@softlinkinfotech.in>
2014-09-17 18:06:39 +0530

    error solving

--- src/android/RingtonePicker.java
+++ src/android/RingtonePicker.java
@@ -27,7 +27,7 @@ import android.media.RingtoneManager;
 import org.apache.cordova.PluginResult;
 import android.util.Log;
 import android.content.Intent;
-import android.provider.Settings.System;
+import android.provider.Settings;
 /**
  * This class provides access to vibration on the device.
  */
