db9a0b315de79944b707a951aa545bbb2a2294c6
sanalt <sanal.thomas@softlinkinfotech.in>
2014-09-17 16:50:18 +0530

    rigntonepicker
    rigntonepicker

---
+++ src/android/RingtonePicker.java
@@ -0,0 +1,93 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+package org.apache.cordova.ringtonepicker;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.content.Context;
+import android.media.RingtoneManager;
+import org.apache.cordova.PluginResult;
+/**
+ * This class provides access to vibration on the device.
+ */
+public class RingtonePicker extends CordovaPlugin {
+
+	private CallbackContext callbackContext = null;
+    /**
+     * Constructor.
+     */
+    public RingtonePicker() {
+    }
+
+    @Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		this.callbackContext = callbackContext;
+    	if (action.equals("getRingtone")) { 
+        	Log.d("customPlugin", " getRingtone ");
+        	this.getRingtone();
+        	return true;
+    	}
+   		else {
+        	return false;
+   	 	}
+	}
+
+    public Object getRingtone() {
+        
+    }
+    
+    public void getRingtone()
+	{
+		Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER); 
+   		intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select ringtone for notifications:");
+    	intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
+    	intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
+    	notification_uri = Settings.System.getString(cordova.getActivity().getContentResolver(), Settings.System.NOTIFICATION_SOUND);
+    	if (notification_uri == null)
+    	{
+        	intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (Uri)null);
+    	}
+    	else
+    	{
+        	intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, Uri.parse(notification_uri));
+    	}
+    	cordova.getActivity().startActivityForResult(intent, 5);
+}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+     	Log.i("customPlugin", "Calling onActivityResult");
+    	if (resultCode == Activity.RESULT_OK && requestCode == 5)
+    	{
+        	Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+       	 	Log.i("customPlugin", "I picked this ringtone " + uri);
+        	if (uri != null)
+        	{
+        		Log.i("customPlugin", "Setting ringtone to  " + notification_uri);
+        		PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, uri);
+        		pluginResult.setKeepCallback(true);
+        		this.callbackContext.sendPluginResult(pluginResult);
+        	}
+    	}
+	}
+
+}
